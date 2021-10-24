1d7c2b364a972ddb9c616deb64d090fe3de61f6c
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2021-10-24 03:35:10 +0000

    feat: basic confi to link provider and connect

---
+++ redux/src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SongList = () => {
+  return (
+    <div>
+      <h1>Song List</h1>
+      Not yet configured
+    </div>
+  );
+};
+
+export default connect()(SongList);
