6691a34de7fad0e5a246d7589265e5cadb6aa464
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2021-10-24 04:54:56 +0000

    chore: Working UI

--- redux/src/components/SongList.js
+++ redux/src/components/SongList.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongList = () => {
-  return (
-    <div>
-      <h1>Song List</h1>
-      Not yet configured
-    </div>
-  );
-};
+class SongList extends React.Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
+  }
+}
 
 const mapStateToProps = (state) => {
-  console.log(state);
-
-  return state;
+  return { songs: state.songs };
 };
 
 export default connect(mapStateToProps)(SongList);
